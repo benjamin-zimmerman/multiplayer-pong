3b59e3164497c1b9e50ebfd7fa6800fe0f582c9f
benjamin-zimmerman <benjamin.zimmerman1@gmail.com>
2020-11-22 19:50:08 -0800

    Update StaticGame.js
    Move to outside update()

--- public/StaticGame.js
+++ public/StaticGame.js
@@ -19,6 +19,24 @@ class Game {
 		this.ball_velocity = [MIN_SPEED, 0];
 	}
 
+	
+	if (
+			this.ball[1] < this.players[this.player2].pos + 10 &&
+			this.ball[1] + 2 > this.players[this.player2].pos - 10 &&
+			this.ball[0] > 94 &&
+			this.ball[0] < 98
+		) {play_boing();
+		  }
+	else if (
+			this.ball[1] < this.players[this.player1].pos + 10 &&
+			this.ball[1] + 2 > this.players[this.player1].pos - 10 &&
+			this.ball[0] < 6 &&
+			this.ball[0] > 2
+		) {play_boing();
+		  }
+	
+	
+	
 	//Updates game_state and calculates ball position and velocity
 	
 	update() {
@@ -62,7 +80,7 @@ class Game {
 			);
 			this.ball_velocity[1] = -normalizedRelativeIntersectionY;
 			//audio.addEventListener("canplaythrough", function() {audio.play(); })
-			play_boing();
+			
 			
 			
 		} else if (
