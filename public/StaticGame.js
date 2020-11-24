26db63ac5eafad24c8e7bcdc5b2ce82c22110d3a
Unknown <benjamin.zimmerman1@gmail.com>
2020-11-23 17:06:14 -0800

    add back static game

---
+++ public/StaticGame.js
@@ -0,0 +1,115 @@
+const MAX_SPEED = 1.5;
+const MIN_SPEED = 1;
+const MAX_SCORE = 10;
+var audio = new Audio("https://raw.githubusercontent.com/benjamin-zimmerman/multiplayer-pong/dev/public/boing.mp3");
+//const boingSOUND = document.getElementbyID("boing");
+function play_boing() {
+	document.getElementbyID('boing').play();
+}
+
+class Game {
+	constructor(id, username, id2, username2) {
+		this.ball_vel = 0.5;
+		this.player1 = id;
+		this.player2 = id2;
+		this.players = {};
+		this.players[id] = { name: username.toString(), pos: 50, score: 0 };
+		this.players[id2] = { name: username.toString(), pos: 50, score: 0 };
+		this.ball = [20, 50];
+		this.ball_velocity = [MIN_SPEED, 0];
+		this.sound1 = audio;
+	}
+	
+	update_sound() {
+	if (
+		this.ball[1] < this.players[this.player2].pos + 10 &&
+		this.ball[1] + 2 > this.players[this.player2].pos - 10 &&
+		this.ball[0] > 94 &&
+		this.ball[0] < 98
+	) {play_boing();
+	  } else if (
+		this.ball[1] < this.players[this.player1].pos + 10 &&
+		this.ball[1] + 2 > this.players[this.player1].pos - 10 &&
+		this.ball[0] < 6 &&
+		this.ball[0] > 2
+	    ) {play_boing();
+		}
+	this.update();
+	}
+	
+	
+	//Updates game_state and calculates ball position and velocity
+	
+	update() {
+		//First consider cases where nothing needs to change
+		this.ball[0] += this.ball_velocity[0];
+		this.ball[1] += this.ball_velocity[1];
+		
+		//Consider cases where a player scores
+		if (this.ball[0] >= 100) {
+			this.players[this.player1].score++;
+			this.reset(1);
+		} else if (this.ball[0] <= 0) {
+			this.players[this.player2].score++;
+			this.reset(2);
+		}
+
+		if (this.ball[1] >= 100) {
+			this.ball_velocity[1] *= -1;
+			this.ball[1] = 99;
+		} else if (this.ball[1] <= 0) {
+			this.ball_velocity[1] *= -1;
+			this.ball[1] = 1;
+		}
+
+		//Ugly conditionals, but eh not familiar with javascript syntactically and it works
+		//Consider cases when a player blocks with their paddle
+		if (
+			this.ball[1] < this.players[this.player2].pos + 10 &&
+			this.ball[1] + 2 > this.players[this.player2].pos - 10 &&
+			this.ball[0] > 94 &&
+			this.ball[0] < 98
+		) {
+			this.ball[0] = 94;
+			var relativeIntersectY =
+				this.players[this.player2].pos - this.ball[1] - 1;
+			var normalizedRelativeIntersectionY = relativeIntersectY / 10;
+			this.ball_velocity[0] = -(
+				(1 - Math.abs(normalizedRelativeIntersectionY)) *
+				(MAX_SPEED - MIN_SPEED) +
+				MIN_SPEED
+			);
+			this.ball_velocity[1] = -normalizedRelativeIntersectionY;
+			//audio.addEventListener("canplaythrough", function() {audio.play(); })
+			
+			
+			
+		} else if (
+			this.ball[1] < this.players[this.player1].pos + 10 &&
+			this.ball[1] + 2 > this.players[this.player1].pos - 10 &&
+			this.ball[0] < 6 &&
+			this.ball[0] > 2
+		) {
+			this.ball[0] = 6;
+			var relativeIntersectY =
+				this.players[this.player1].pos - this.ball[1] - 1;
+			var normalizedRelativeIntersectionY = relativeIntersectY / 10;
+			var normalizedRelativeIntersectionY = relativeIntersectY / 10;
+			this.ball_velocity[0] =
+				(1 - Math.abs(normalizedRelativeIntersectionY)) *
+				(MAX_SPEED - MIN_SPEED) +
+				MIN_SPEED;
+			this.ball_velocity[1] = -normalizedRelativeIntersectionY;
+		}
+	}
+
+	reset(player) {
+		if (player == 1) {
+			this.ball = [60, 50];
+			this.ball_velocity = [-MIN_SPEED / 3, 0];
+		} else {
+			this.ball = [40, 50];
+			this.ball_velocity = [MIN_SPEED / 3, 0];
+		}
+	}
+}
